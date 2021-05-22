946ecb692590f5e3700d4aef2ea2f12627afc36c
Jared Gilleland <ejgilleland@gmail.com>
2021-05-22 13:41:48 -0700

    Completed POST route for sgt-back-end

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -28,6 +28,34 @@ app.get('/api/grades', (req, res, next) => {
     });
 });
 
+app.post('/api/grades', (req, res, next) => {
+  if (!(('course' in req.body) && ('name' in req.body) && ('score' in req.body))) {
+    res.status(400).send({ error: 'Please make sure to include a course, name, and score' });
+  } else {
+    if (req.body.score >= 0 && req.body.score <= 100) {
+      const sql = `
+      insert into "grades" ("course", "name", "score")
+      values ($1, $2, $3)
+      returning *;
+      `;
+      const values = [req.body.course, req.body.name, req.body.score];
+      db
+        .query(sql, values)
+        .then(result => {
+          res.status(201).json(result.rows[0]);
+        })
+        .catch(err => {
+          console.error(err);
+          res.status(500).json({
+            error: 'An unexpected error occurred'
+          });
+        });
+    } else {
+      res.status(400).send({ error: 'score must be a nonnegative integer less than 100' });
+    }
+  }
+});
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on port 3000');
